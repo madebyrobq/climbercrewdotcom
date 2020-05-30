5a4b1100fb86a5623b7b77acf16babf71bda96d5
rob <rob@angle.fish>
2020-05-30 15:14:42 -0700

    added anglefish brand

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './images/ClimberCrewLogo.png';
+import climbercrewlogo from './images/ClimberCrewLogo-small.png';
+import anglefishlogo from './images/ANGLEFISH_logo-small.png';
 import bg from './images/backgroundimage.png';
 import './App.css';
 
@@ -7,21 +8,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>a game by Rob Quinn</p>
-        
+        <img src={climbercrewlogo} className="App-logo" alt="" />        
       </header>
       <body>
         <p className="sky"><b>Climber Crew is a rock climbing adventure game with delightful ragdoll physics.</b> Play solo and master each route on the map or race your friends in multiplayer for up to 4 players. Features endless rock walls to ascend, a diverse cast of climbers, and many fun surprises as you go.</p>
 
-        <h2 className="sky">Coming summer 2020</h2>
+        <h2 className="sky">In development</h2>
         <h3 className="sky">Expected release on iPad, iPhone, PC, Switch, and more...</h3>
 
         <p>more information and presskit coming soon</p>
 
         <a href="http://twitter.com/RobQuiiinn">Follow on Twitter</a>
+        
+        <img src={anglefishlogo} className="Co-logo" alt="" />        
 
-        <p>Copyright &copy; 2015-2019 Robert Quinn</p>
+
+        <p>Copyright &copy; 2020 Robert Quinn and ANGLEFISH™. All rights reserved.</p>
       </body>
     </div>
   );
